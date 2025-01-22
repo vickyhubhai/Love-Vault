665bec0a819fc615bfd8660a9665019c7d952c15
rohitchauhan0 <beatfeelrc121@gmail.com>
2025-01-22 16:45:18 +0530

    third commit

--- src/app/_components/chat/page.js
+++ src/app/_components/chat/page.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
 
 const ChatRoom = ({ roomId }) => {
-      const socket = useMemo(() => io("https://lovetunes-1.onrender.com"), []);
+      const socket = useMemo(() => io("https://lovetunes-2.onrender.com"), []);
     // const socket = useMemo(() => io("http://localhost:3001"), []);
     const [message, setMessage] = useState("");
     const [chat, setChat] = useState([]);
